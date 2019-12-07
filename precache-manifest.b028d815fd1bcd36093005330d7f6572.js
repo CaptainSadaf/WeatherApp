89a13e2ac62812fdf5300be42e5f4ae6b4e07cc3
Sadaf Adrees <sadrees@CS.NET>
2019-12-07 05:12:13 +0500

    Updates

---
+++ precache-manifest.b028d815fd1bcd36093005330d7f6572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5498568c15cbe9285ec04507ff72310c",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "16ff81d87308658b736d",
+    "url": "/weatherApp/static/css/main.0c71fef3.chunk.css"
+  },
+  {
+    "revision": "b7e53d51b7b255447434",
+    "url": "/weatherApp/static/js/2.e9415ed6.chunk.js"
+  },
+  {
+    "revision": "16ff81d87308658b736d",
+    "url": "/weatherApp/static/js/main.7342ab96.chunk.js"
+  },
+  {
+    "revision": "2895d0d452a116ca77eb",
+    "url": "/weatherApp/static/js/runtime-main.39750cc0.js"
+  }
+]);
